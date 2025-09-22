e9a5b9c39990d170cce96521eb4d1daf9fd08c71
ZherAr7xa <zherlicina.arina@gmail.com>
2025-09-22 22:27:08 +0300

    Create setup.py
    Добавил setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="astro-bot",
+    version="0.1.0",
+    install_requires=[
+        "aiogram==2.25.1",
+        "requests==2.31.0",
+        "python-dotenv==1.0.1"
+    ],
+    packages=[""],
+)
